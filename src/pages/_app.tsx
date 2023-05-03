c89d8d784a965e1a3623908fa42e5307fa51701b
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2023-05-03 23:02:09 +0900

    ✨ add liff init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,39 @@
+import type { Liff } from "@line/liff/exports";
 import "destyle.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import "~/styles/global.scss";
 
-const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Element => (
-  <>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} key={routerProp.asPath} />
-  </>
-);
+const MyApp = ({ Component, pageProps, router: routerProp }: AppProps): JSX.Element => {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff")
+      .then((liff) => liff.default)
+      .then((liff) => {
+        liff
+          .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID || "" })
+          .then(() => {
+            setLiffObject(liff);
+          })
+          .catch((error: Error) => {
+            setLiffError(error.toString());
+          });
+      });
+  }, []);
+
+  // eslint-disable-next-line no-param-reassign
+  pageProps.liff = liffObject;
+  // eslint-disable-next-line no-param-reassign
+  pageProps.liffError = liffError;
+  return (
+    <>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} key={routerProp.asPath} />
+    </>
+  );
+};
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import type { Liff } from "@line/liff/exports";
 import type { NextPage } from "next";
 import Layout from "~/components/layout";
 
-const Home: NextPage = () => (
+// eslint-disable-next-line react/prop-types
+const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({ liff, liffError }) => (
   <Layout>
-    <h1>Hello World</h1>
+    <h1>Hello World!</h1>
+    <p>This is a test app with liff.</p>
+    {liff && <p>LIFF init succeeded.</p>}
+    {liffError && (
+      <>
+        <p>LIFF init failed.</p>
+        <p>
+          <code>{liffError}</code>
+        </p>
+      </>
+    )}
   </Layout>
 );
 
